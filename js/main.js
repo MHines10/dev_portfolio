cd9558666d603163e2f7d3bd38055ae0a884f18f
MHines10 <cd.mhines@gmail.com>
2023-08-25 20:40:22 -0400

    updated mobile menu button style

--- js/main.js
+++ js/main.js
@@ -28,14 +28,14 @@ $(document).ready(function () {
     });
 
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Designer", "Freelancer"],
+        strings: ["Developer", "Freelancer", "Designer"],
         typeSpeed: 120,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Designer", "Freelancer"],
+        strings: ["Developer", "Freelancer", "Designer"],
         typeSpeed: 120,
         backSpeed: 60,
         loop: true
